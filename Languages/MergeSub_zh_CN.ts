5e228d2169e1d6a198d26fd1f3564c9dece3ef7f
LichenKass <yuanchengwei_sz@163.com>
2025-09-21 00:04:16 +0800

    Add files via upload

---
+++ Languages/MergeSub_zh_CN.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="60"/>
+        <source>选择字幕文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="61"/>
+        <source>字幕文件 (*.srt *.ass *.vtt *.ssa *.sub)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="1056"/>
+        <source>语言:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
